7fff974e695cf10d4e3e0856f39c3cf3bee528b4
CanadaApollo6 <riel.stamand@gmail.com>
2020-08-19 18:28:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://canadaapollo6.github.io/face-recognition-brain/"
-  );
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
